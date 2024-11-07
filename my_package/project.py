94329afedc26a3ca777a4024b839a1e15e20c34a
nekarbalraj <nekarbalraj@gmail.com>
2024-11-07 10:20:31 +0530

    second commit

--- my_package/project.py
+++ my_package/project.py
@@ -19,18 +19,3 @@ def divide(x, y):
         raise ValueError('Can not divide by zero!')
     return x / y
 
-while True:
-    print("1.Addition\t2.Substraction\n3.multiply\t4.divide")
-    ch=int(input("Enter your Choice"))
-    match ch:
-        case 1:
-            print(add(5,6))
-        case 2:
-            print(subtract(6,3))
-        case 3:
-            print(multiply(3,3))
-        case 4:
-            print(divide(3,3))
-        case _:
-            print("Invalid Choice")
-            break
